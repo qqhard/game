3086d0da8a400992bbcb086cb19ba686aaec965b
hard <4533824417@qq.com>
2016-03-11 17:16:49 +0800

    web

---
+++ web/src/games.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import GameList from './components/game_list/game_list.js'
+
+class Games extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {data: []};
+    }
+    componentDidMount() {
+        var _this = this;
+        $.get('/games',function(data){
+            _this.setState({data: data});
+        });
+    }
+    render() {
+        return (
+            <div>
+                <GameList data={this.state.data}/>
+            </div>
+        )
+    }
+}
+
+export default Games;
