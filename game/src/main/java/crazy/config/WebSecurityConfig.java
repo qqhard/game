3747bafe6bb09ead7586d1585f32de64a38bf49e
hard <4533824417@qq.com>
2016-03-18 06:55:55 +0800

    user layout change

--- game/src/main/java/crazy/config/WebSecurityConfig.java
+++ game/src/main/java/crazy/config/WebSecurityConfig.java
@@ -25,7 +25,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
       	http
       		.authorizeRequests()
-				.antMatchers("/gamecheck").permitAll()
+				.antMatchers("/games").permitAll()
 				.anyRequest()
 				.authenticated();
       	
