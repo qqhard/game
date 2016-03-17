6f844868fbecc93eaa7894a427745815c6a4f6a1
hard <4533824417@qq.com>
2016-03-18 02:12:21 +0800

    games done

--- user/src/main/java/crazy/dao/UserRepository.java
+++ user/src/main/java/crazy/dao/UserRepository.java
@@ -2,6 +2,7 @@ package crazy.dao;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import crazy.vo.User;
 
