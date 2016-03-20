8090c302b01be04dd36a484c22204a3013bf0136
hard <453824417@qq.com>
2016-03-20 16:52:06 +0800

    duoshuo debug

--- web/src/components/game_comment/game_comment.js
+++ web/src/components/game_comment/game_comment.js
@@ -4,16 +4,20 @@ class GameComment extends React.Component {
     constructor(props) {
         super(props);
     }
-    toggleDuoshuoComments(container){
-        var el = document.createElement('div');//该div不需要设置class="ds-thread"
-        el.setAttribute('data-thread-key', this.props.game.gamename);//必选参数
-        el.setAttribute('data-url', window.location);//必选参数
-        DUOSHUO.EmbedThread(el);
-        jQuery(container).append(el);
-    }
+
     componentDidMount() {
-        this.toggleDuoshuoComments($("#game_comment"));
+        var _this = this;
+        function toggleDuoshuoComments(container){
+            var el = document.createElement('div');//该div不需要设置class="ds-thread"
+            el.setAttribute('data-thread-key', _this.props.game.gamename);//必选参数
+            el.setAttribute('data-url', window.location);//必选参数
+            DUOSHUO.EmbedThread(el);
+            jQuery(container).append(el);
+        }
+        toggleDuoshuoComments($("#game_comment"));
+
     }
+
     render() {
         return (
             <div id="game_comment">
