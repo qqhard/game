57f12717294b22f3b7b1a6fad97e10ea9a6c711b
hard <453824417@qq.com>
2016-04-10 20:01:31 +0800

    game step change

--- web/src/components/game_steps/game_steps.js
+++ web/src/components/game_steps/game_steps.js
@@ -10,36 +10,56 @@ class GameSteps extends React.Component {
         super(props);
     }
 
-    getStatus(step) {
-        var process = this.props.game.step + 1;
-        if (process == step)return 'process';
-        if (process < step)return 'wait';
-        if (process > step)return 'finish';
+    getStatus() {
+        if(this.props.game.step == 0){
+            return ['process','wait','wait','wait','wait'];
+        }else if(this.props.game.step == 1){
+            return ['finish','process','wait','wait','wait'];
+        }else if(this.props.game.step == 2){
+            const nowTime = Date.parse(new Date());
+            if(this.props.game.startTime == 0 || this.props.game.dueTime == 0 || this.props.game.endTime == 0){
+                return ['finish','finish','wait','wait','wait'];
+            }
+            if(nowTime < this.props.game.startTime){
+                return ['finish','finish','wait','wait','wait'];
+            }else if(nowTime < this.props.game.dueTime){
+                return ['finish','finish','process','wait','wait'];
+            }else if(nowTime < this.props.game.endTime){
+                return ['finish','finish','finish','finish','wait'];
+            }else if(nowTime > this.props.game.endTime){
+                return ['finish','finish','finish','finish','finish'];
+            }
+        }
     }
 
     render() {
-        var submitTime = this.props.game.submitTime;
-        var acceptTime = this.props.game.acceptTime;
+        const submitTime = this.props.game.submitTime;
+        const acceptTime = this.props.game.acceptTime;
+        const startTime = this.props.game.startTime;
+        const dueTime = this.props.game.dueTime;
+        const endTime = this.props.game.endTime;
+        const nowTime = Date.parse(new Date());
+        const status = this.getStatus();
         const steps = [{
-            status: this.getStatus(1),
+            status: status[0],
             title: '赛事提交',
             description: submitTime > 0 ? timeFormat(submitTime) : '赛事提交'
         }, {
-            status: this.getStatus(2),
+            status: status[1],
             title: '赛事审核',
             description: acceptTime > 0 ? timeFormat(acceptTime) : '赛事审核'
         }, {
-            status: this.getStatus(3),
-            title: '赛事报名',
-            description: '赛事报名'
+            status: status[2],
+            title: '报名开始',
+            description: startTime > 0 ? timeFormat(startTime) : '报名开始'
         }, {
-            status: this.getStatus(4),
-            title: '赛事开始',
-            description: '赛事开始'
+            status: status[3],
+            title: '报名截止',
+            description: dueTime > 0 ? timeFormat(dueTime) : '报名截止'
         }, {
-            status: this.getStatus(5),
+            status: status[4],
             title: '赛事结束',
-            description: '赛事结束'
+            description: endTime > 0 ? timeFormat(endTime) : '赛事结束'
         }].map(function (s, i) {
             return (
                 <Step key={i} title={s.title} status={s.status} description={s.description}/>
@@ -47,7 +67,7 @@ class GameSteps extends React.Component {
         });
         console.log('step render');
         return (
-            <Steps direction="vertical" >{steps}</Steps>
+            <Steps direction="vertical">{steps}</Steps>
         )
     }
 }
