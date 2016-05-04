23a1c7a10dddbab9507dd29b266fb315d9f81b60
LackOfDream <galaxy0419@gmail.com>
2016-05-04 23:02:38 +0800

    update links

--- web/src/components/common/footer.js
+++ web/src/components/common/footer.js
@@ -10,7 +10,7 @@ class Footer extends React.Component {
                         <dl className="col-sm-3 site-link">
                             <dt>网站相关</dt>
                             <dd><a href="">关于我们</a></dd>
-                            <dd><a href="">反馈bug</a></dd>
+                            <dd><a href="mailto:root@acm.hitwh.edu.cn">反馈bug</a></dd>
                             <dd><a href="">帮助中心</a></dd>
                         </dl>
                         <dl className="col-sm-3 site-link">
@@ -22,24 +22,24 @@ class Footer extends React.Component {
                         <dl className="col-sm-3 site-link">
                             <dt>友情链接</dt>
                             <dd><a href="http://nphard.me" target="_blank">hard's blog</a></dd>
-                            <dd><a href="https://lackofdream.github.io" target="_blank">lack of dream</a></dd>
+                            <dd><a href="https://lackofdream.github.io" target="_blank">LackOfDream</a></dd>
                             <dd><a href="http://littlebubble.me" target="_blank">little bubble</a></dd>
                         </dl>
                         <dl className="col-sm-3 site-link" id="license">
                             <dt>我们用到的技术</dt>
                             <dd className="tech">
-                                <a href="" target="_blank">Bootstrap</a><span> </span>
-                                <a href="" target="_blank">jQuery</a><span> </span>
-                                <a href="" target="_blank">AntDesign</a><span> </span> 
-                                <a href="" target="_blank">React</a><span> </span>
-                                <a href="" target="_blank">react-router</a><span> </span>
-                                <a href="" target="_blank">Webpack</a><span> </span>
-                                <a href="" target="_blank">Docker</a><span> </span>
-                                <a href="" target="_blank">Spring Boot</a><span> </span>
-                                <a href="" target="_blank">Node</a><span> </span>
-                                <a href="" target="_blank">NPM</a><span> </span>
-                                <a href="" target="_blank">Bable</a><span> </span>
-                                <a href="" target="_blank">MongoDB</a><span> </span>
+                                <a href="http://getbootstrap.com/" target="_blank">Bootstrap</a><span> </span>
+                                <a href="https://jquery.com/" target="_blank">jQuery</a><span> </span>
+                                <a href="http://ant.design/" target="_blank">AntDesign</a><span> </span>
+                                <a href="https://facebook.github.io/react/" target="_blank">React</a><span> </span>
+                                <a href="https://github.com/reactjs/react-router" target="_blank">react-router</a><span> </span>
+                                <a href="https://webpack.github.io/" target="_blank">Webpack</a><span> </span>
+                                <a href="https://www.docker.com/" target="_blank">Docker</a><span> </span>
+                                <a href="http://projects.spring.io/spring-boot/" target="_blank">Spring Boot</a><span> </span>
+                                <a href="https://nodejs.org/en/" target="_blank">Node</a><span> </span>
+                                <a href="https://www.npmjs.com/" target="_blank">NPM</a><span> </span>
+                                <a href="https://babeljs.io/" target="_blank">Bable</a><span> </span>
+                                <a href="https://www.mongodb.org/" target="_blank">MongoDB</a><span> </span>
                             </dd>
                         </dl>
                     </div>
